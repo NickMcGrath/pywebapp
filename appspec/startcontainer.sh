d619c6a4c7842bc322494a4b61ace75db32ef603
Nicholas McGrath <47646061+NickMcGrath@users.noreply.github.com>
2020-11-10 10:54:13 -0800

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 491244813759.dkr.ecr.us-west-2.amazonaws.com
-docker pull 491244813759.dkr.ecr.us-west-2.amazonaws.com/comp4964:latest || {
+docker pull 491244813759.dkr.ecr.us-west-2.amazonaws.com/nickmcgrath/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
 }
-docker run --name pywebapp -p 80:8080 --detach 491244813759.dkr.ecr.us-west-2.amazonaws.com/comp4964:latest
+docker run --name pywebapp -p 80:8080 --detach 491244813759.dkr.ecr.us-west-2.amazonaws.com/nickmcgrath/pywebapp:latest
